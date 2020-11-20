2af492476322eb22387ac6f774541d7e9a35cc0f
stuart <qmstuart@gmail.com>
2020-11-20 18:17:21 +0000

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to maze-solver
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/exfn/maze_solver_test.clj
@@ -0,0 +1,7 @@
+(ns exfn.maze-solver-test
+  (:require [clojure.test :refer :all]
+            [exfn.maze-solver :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
